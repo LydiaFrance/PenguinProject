930d3e1dd07ed5b0f62425d4e8c7fae0b9e977d3
LydiaFrance <lfrance@turing.ac.uk>
2022-11-11 18:04:37 +0000

    Remove figures

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reproducible R Tutorial Using Palmer Penguins Dataset
+
+This GitHub repository contains an example R folder and subfolders, feel free to look through. 
+
+This file has all the code from the notebooks and sources from files inside "functions".
+
+This file has all code from all the notebooks and doesn't source any other files, the functions are inside the file. 
+
--- functions/libraries.r
+++ functions/libraries.r
@@ -1,3 +1,22 @@
+## ---------------------------
+##
+## Script name: libraries.r
+##
+## Purpose of script: 
+##      Loading necessary libraries
+##
+## Author: Dr. Lydia France
+##
+## Date Created: 2022-11-10
+##
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
 library(palmerpenguins)
 library(ggplot2)
 suppressPackageStartupMessages(library(janitor))
