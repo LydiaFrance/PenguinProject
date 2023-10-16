c99d8d271cc5da814065a8600355495f25e96636
Lydia France <lfrance@turing.ac.uk>
2023-10-16 16:54:06 +0100

    Add example

--- functions/.ipynb_checkpoints/libraries-checkpoint.r
+++
@@ -1,7 +0,0 @@
-library(palmerpenguins)
-library(ggplot2)
-suppressPackageStartupMessages(library(janitor))
-suppressPackageStartupMessages(library(dplyr))
-library(tidyr)
-library(ragg)
-library(svglite)
